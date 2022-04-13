372225793da6a21269394e02897015e26752bced
yuOShin010 <angelsave020@gmail.com>
2022-04-13 21:35:26 +0800

    changes (loginadmin.php, loginpage.css)

--- php/loginAdmin.php
+++ php/loginAdmin.php
@@ -11,7 +11,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/loginpage.css">
-    <link rel="stylesheet" href="../css/sizing/quiries.css">
     <title>Login</title>
 </head>
 
